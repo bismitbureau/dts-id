08b824b07c135fa2f7fa01d9dcd48050a7cb9b40
ciferivalle <nataprawiraf@gmail.com>
2019-02-17 13:37:50 +0700

    adding board of directors container

--- resources/views/bod.blade.php
+++ resources/views/bod.blade.php
@@ -2,9 +2,22 @@
 
 @section('content')
 
-    <h1>Board of Directors</h1>
+    <h1 class="f-futura">Board of Directors</h1>
 
     <hr>
+
+    <div class="row mb-5">
+        <div class="col-md-12">
+            <h4 class="f-futura">Director</h4>
+            <h5>Anis Baswedan, Msc, PhD</h5> 
+        </div>
+        <div class="col-lg-2 col-md-4 col-sm-12">
+            <img class="img-responsive bod" src="{{ asset('images/bod/dummy-bod.jpg') }}" alt="">
+        </div>
+        <div class="col-lg-10 col-md-8 col-sm-12">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis distinctio ullam esse ducimus tempore praesentium! Alias dolore molestias vitae corporis.</p>
+        </div>
+    </div>
 
     <div class="row mb-5">
         <div class="col-md-12">
@@ -19,24 +32,14 @@
         </div>
     </div>
 
-    <div class="row mb-5">
-            <div class="col-md-12">
-                <h4>Director</h4>
-                <h5>Anis Baswedan, Msc, PhD</h5> 
-            </div>
-            <div class="col-lg-2 col-md-4 col-sm-12">
-                <img class="img-responsive bod" src="{{ asset('images/bod/dummy-bod.jpg') }}" alt="">
-            </div>
-            <div class="col-lg-10 col-md-8 col-sm-12">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis distinctio ullam esse ducimus tempore praesentium! Alias dolore molestias vitae corporis.</p>
-            </div>
-        </div>
-
     
 
     <style>
         .bod {
             max-width: 150px;
+        }
+        h4 {
+            font-family: "Futura";
         }
     </style>
 
