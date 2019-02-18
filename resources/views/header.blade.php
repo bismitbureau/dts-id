1689c2aa8db8bdbe08ede77075bfa64c7d77ecf5
Khalis Murfid <khalismurfid@Khaliss-MacBook-Pro.local>
2019-02-18 23:06:45 +0700

    Add transparency to header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar sticky-top navbar-expand-lg navbar-dark bg-dark shift ">
+<nav class="navbar navbar sticky-top navbar-expand-lg navbar-dark bg-dark shift " id="navbar">
   <a class="navbar-brand" href="#">LOGO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
